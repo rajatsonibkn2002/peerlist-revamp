8179d693bd4451330e1512ed22ce7aecf5f5f86b
rajatsonibkn2002 <rajatsonibkn2002@gmail.com>
2022-10-27 16:41:41 +0530

    added mobile css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Generator from './components/Generator';
+import { useState } from 'react';
 
 function App() {
+  const [stars, setStars] = useState(0);
+  const [forks, setForks] = useState(0);
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = false;
+  
+  xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      setStars(JSON.parse(this.responseText).watchers_count);
+      setForks(JSON.parse(this.responseText).forks_count);
+    }
+  });
+  
+  xhr.open("GET", "https://api.github.com/repos/vinitshahdeo/peerlist-readme-badge");
+  
+  xhr.send();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='peerlist'>
+      <Navbar star={stars} fork={forks} />
+      <Generator />
     </div>
   );
 }
